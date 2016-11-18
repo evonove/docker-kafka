43b933b748e10b41396141789d3a3ea83377b621
Federico Frenguelli <synasius@gmail.com>
2016-11-18 10:51:16 +0100

    Changed variable substitution rule for KAFKA_ADVERTISED_LISTENERS to allow set an empty value

--- start.sh
+++ start.sh
@@ -5,7 +5,7 @@ IP=$(grep "\s${HOSTNAME}$" /etc/hosts | head -n 1 | awk '{print $1}')
 
 
 cat /opt/kafka/config/server.properties.tmpl | sed \
-  -e "s|{{KAFKA_ADVERTISED_LISTENERS}}|${KAFKA_ADVERTISED_LISTENERS:-$IP}|g" \
+  -e "s|{{KAFKA_ADVERTISED_LISTENERS}}|${KAFKA_ADVERTISED_LISTENERS-$IP}|g" \
   -e "s|{{ZOOKEEPER_CONNECT}}|${ZOOKEEPER_CONNECT}|g" \
    > /opt/kafka/config/server.properties
 
