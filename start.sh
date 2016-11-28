ce10d023effaf0dfecae5baeb73b1672cba1c72a
Federico Frenguelli <synasius@gmail.com>
2016-11-28 17:22:16 +0100

    Added ZOOKEEPER_CONNECTION_TIMEOUT_MS to environ variables

--- start.sh
+++ start.sh
@@ -7,6 +7,7 @@ IP=$(grep "\s${HOSTNAME}$" /etc/hosts | head -n 1 | awk '{print $1}')
 cat /opt/kafka/config/server.properties.tmpl | sed \
   -e "s|{{KAFKA_ADVERTISED_LISTENERS}}|${KAFKA_ADVERTISED_LISTENERS-$IP}|g" \
   -e "s|{{ZOOKEEPER_CONNECT}}|${ZOOKEEPER_CONNECT}|g" \
+  -e "s|{{ZOOKEEPER_CONNECTION_TIMEOUT_MS}}|${ZOOKEEPER_CONNECTION_TIMEOUT_MS:-6000}|g" \
    > /opt/kafka/config/server.properties
 
 echo "Starting kafka"
