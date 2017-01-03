467c5db3ce7d6dea0a34a2badf23d7f71589842b
Federico Frenguelli <synasius@gmail.com>
2017-01-03 18:15:21 +0100

    Changed KAFKA_ADVERTISED_LISTENERS env variable so that it can be used to set multiple listeners

--- start.sh
+++ start.sh
@@ -2,10 +2,10 @@
 
 
 IP=$(grep "\s${HOSTNAME}$" /etc/hosts | head -n 1 | awk '{print $1}')
-
+DEFAULT_ADVERTISED_LISTENER=PLAINTEXT://$IP:9092
 
 cat /opt/kafka/config/server.properties.tmpl | sed \
-  -e "s|{{KAFKA_ADVERTISED_LISTENERS}}|${KAFKA_ADVERTISED_LISTENERS-$IP}|g" \
+  -e "s|{{KAFKA_ADVERTISED_LISTENERS}}|${KAFKA_ADVERTISED_LISTENERS-$DEFAULT_ADVERTISED_LISTENER}|g" \
   -e "s|{{ZOOKEEPER_CONNECT}}|${ZOOKEEPER_CONNECT}|g" \
   -e "s|{{ZOOKEEPER_CONNECTION_TIMEOUT_MS}}|${ZOOKEEPER_CONNECTION_TIMEOUT_MS:-6000}|g" \
    > /opt/kafka/config/server.properties
