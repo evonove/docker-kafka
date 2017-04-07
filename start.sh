2ea0fc799a3a1236e37c562b4945f82c4ac10e88
Federico Frenguelli <synasius@gmail.com>
2017-04-07 16:37:05 +0200

    Updated kafka to 0.10.2.0

--- start.sh
+++ start.sh
@@ -5,6 +5,7 @@ IP=$(grep "\s${HOSTNAME}$" /etc/hosts | head -n 1 | awk '{print $1}')
 DEFAULT_ADVERTISED_LISTENER=PLAINTEXT://$IP:9092
 
 cat /opt/kafka/config/server.properties.tmpl | sed \
+  -e "s|{{KAFKA_BROKER_ID}}|${KAFKA_BROKER_ID:--1}|g" \
   -e "s|{{KAFKA_ADVERTISED_LISTENERS}}|${KAFKA_ADVERTISED_LISTENERS-$DEFAULT_ADVERTISED_LISTENER}|g" \
   -e "s|{{ZOOKEEPER_CONNECT}}|${ZOOKEEPER_CONNECT}|g" \
   -e "s|{{ZOOKEEPER_CONNECTION_TIMEOUT_MS}}|${ZOOKEEPER_CONNECTION_TIMEOUT_MS:-6000}|g" \
